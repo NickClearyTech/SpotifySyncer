d2e3118b21d1782d5849d9dca42939b13d2ef91a
Nick Cleary <nicholascleary01@gmail.com>
2023-03-22 01:22:25 -0400

    Fixing frontend initialization

--- frontend/spotifysyncer/src/components/HelloWorld.vue
+++ frontend/spotifysyncer/src/components/HelloWorld.vue
@@ -119,20 +119,18 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { defineComponent } from "vue";
 
-@Options({
+export default defineComponent({
+  name: "HelloWorld",
   props: {
     msg: String,
   },
-})
-export default class HelloWorld extends Vue {
-  msg!: string;
-}
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style scoped>
 h3 {
   margin: 40px 0 0;
 }
--- frontend/spotifysyncer/src/main.ts
+++ frontend/spotifysyncer/src/main.ts
@@ -1,9 +1,28 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
-import "./plugins/bootstrap-vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: "dark",
+  },
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
