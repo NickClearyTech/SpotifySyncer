64dc33050ee6c2d1a0cc80e9a32ac2c702750f5a
Nick Cleary <nicholascleary01@gmail.com>
2023-04-01 16:23:59 -0400

    Added flow code checking

--- frontend/spotifysyncer/src/components/CallBack.vue
+++ frontend/spotifysyncer/src/components/CallBack.vue
@@ -6,32 +6,39 @@
 </template>
 
 <script lang="ts">
-import { getCallback, codeToToken, spotifyTokenToSyncerToken } from "../service/BackendService";
-import {GetSpotifyTokenFromCode} from "../utils/auth/tokens";
+import {GetSpotifyTokenFromCode, GetSyncerTokenFromSpotifyToken} from "../utils/auth/tokens";
 
 export default {
   name: "CallBack",
   methods: {
     onLoad: async function() {
-      console.log(this.$store.state.spotifyFlowCode)
+      // Validate flow code in the url is the same as we set in the cookie previously
+      if (this.$cookies.get("spotify_flow_code") != this.$route.query.state) {
+        return;
+      }
+
+      // Remove cookie containing the flow code
+      if (!this.$cookies.remove("spotify_flow_code")) {
+        return;
+      }
+
       let code: string = this.$route.query.code;
       if (code == null) {
         return;
       }
 
       // Convert Spotify Code to Spotify Token
-      let proccessedToken: [boolean, string, string] = await GetSpotifyTokenFromCode(code);
-      if (!proccessedToken[0]){
+      let processedToken: [boolean, string, string] = await GetSpotifyTokenFromCode(code);
+      if (!processedToken[0]){
         this.success = false;
-        console.log("Sad")
         return;
       }
 
-      this.$cookies.set("spotify_token", proccessedToken[1], 3599); // Set the cookie to expire one second before the token actually does
-      this.$cookies.set("spotify_refresh_token", proccessedToken[2], (3600 * 24 * 180) - 1);
+      this.$cookies.set("spotify_token", processedToken[1], 3599); // Set the cookie to expire one second before the token actually does
+      this.$cookies.set("spotify_refresh_token", processedToken[2], (3600 * 24 * 180) - 1);
 
       // Convert Spotify Token to Syncer Token
-      let syncerResult: [boolean, string, string] = await spotifyTokenToSyncerToken(proccessedToken[1], "default");
+      let syncerResult: [boolean, string, string] = await GetSyncerTokenFromSpotifyToken(processedToken[1], "default");
       if (!syncerResult[0]){
         this.success = false;
         return;
@@ -39,7 +46,7 @@ export default {
 
       this.$cookies.set("syncer_token", syncerResult[1], 3599);
       this.$cookies.set("syncer_refresh_token", syncerResult[2], (3600 * 24 * 180) - 1); // Expire in 6 months
-      console.log(syncerResult);
+      this.success = true;
     }
   },
   beforeMount(){
--- frontend/spotifysyncer/src/components/HelloWorld.vue
+++ frontend/spotifysyncer/src/components/HelloWorld.vue
@@ -3,12 +3,14 @@ import {generateState} from "../utils/randomGenerator";
 
 export default {
   methods:{
-    loginToSpotify: function(){
+    loginToSpotify: function() {
+      // Get a randomly generated string to use as the state code
       let state: string = generateState();
-      this.$store.commit("setFlowCode", state);
-      console.log(this.$store.state.spotifyFlowCode);
+      // Set cookies to be sure no MoTM attack
+      this.$cookies.set("spotify_flow_code", state);
       let url: string = `https://accounts.spotify.com/authorize?response_type=code&redirect_uri=http://localhost:4436/callback&client_id=a98b190ae81745a68f3b2be525f6b0e7&state=${state}`;
-      // window.location.replace(url);
+      // Redirect to spotify auth page
+      window.location.replace(url);
     }
   }
 };
