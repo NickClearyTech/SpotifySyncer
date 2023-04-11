11207064aab9dffcf6d75ad4f432685a3d7f9cef
Nick Cleary <nicholascleary01@gmail.com>
2023-04-10 23:58:06 -0400

    updates

--- backend/spotifysyncer/scripts/initializer.py
+++ backend/spotifysyncer/scripts/initializer.py
@@ -1,11 +1,9 @@
 from spotifysyncer.utils.initializer_utils import (
     check_and_create_initial_superuser,
-    check_and_create_oauth_application,
-    run_dynamodb_migrations,
+    check_and_create_oauth_application
 )
 
 
 def run():
     check_and_create_initial_superuser()
     check_and_create_oauth_application()
-    run_dynamodb_migrations()
