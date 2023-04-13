2f515ad3cf9685c12add15db0843f4c6dee891fd
Nick Cleary <nicholascleary01@gmail.com>
2023-04-12 23:12:14 -0400

    Updates

--- backend/spotifysyncer/spotifysyncer/models.py
+++ backend/spotifysyncer/spotifysyncer/models.py
@@ -1,3 +1,13 @@
+import django.contrib.auth
 from django.db import models
 
 # Create your models here.
+
+class SpotifyKey(models.Model):
+
+    user = models.OneToOneField(
+        django.contrib.auth.get_user_model(),
+        on_delete=models.DO_NOTHING(),
+        related_name="spotify_keys"
+    )
+    refresh_key = models.BinaryField()
